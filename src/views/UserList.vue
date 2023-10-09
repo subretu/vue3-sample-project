4fab585f74602d1e2333ec7f13712867452eab7f
miyahara <50367145+subretu@users.noreply.github.com>
2023-10-10 00:03:10 +0900

    Add expandable rows

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -4,12 +4,20 @@
       <v-col cols="10">
         <v-sheet color="white" elevation="1">
           <v-data-table
-            show-select
             :headers="headers"
             :items="state.userList"
-            item-value="id"
-            :single-select="false"
+            item-value="user_id"
+            show-expand
+            v-model:expanded="state.expanded"
+            expand-on-click
           >
+            <template v-slot:expanded-row="{ columns, item }">
+              <tr>
+                <td :colspan="columns.length" class="expand-data">
+                  {{ createDisplayExpandData(item.role) }}
+                </td>
+              </tr>
+            </template>
           </v-data-table>
         </v-sheet>
       </v-col>
@@ -21,47 +29,45 @@
 import { reactive, onMounted } from "vue";
 import SampleApiService from "../services/SampleApiService";
 
-type DataList = {
-  id?: string | null | undefined;
-  label?: string | null | undefined;
-  data?: number | null | undefined;
+type UserList = {
+  userName: string;
+  companyName: string;
 };
 
-type state = { userList: DataList[] };
+type state = { userList: UserList[]; expanded: [] };
 
 export default {
   setup() {
     const headers = [
       {
-        title: "ID",
-        key: "id",
+        title: "ユーザー名",
+        key: "user_name",
       },
       {
-        title: "日付",
-        key: "label",
-      },
-      {
-        title: "合計値",
-        key: "data",
+        title: "会社",
+        key: "company_name",
       },
     ];
 
-    const state = reactive<state>({ userList: [] });
-
-    const getDay = async () => {
-      const response = await SampleApiService.get();
-      return response.data;
-    };
+    const state = reactive<state>({ userList: [], expanded: [] });
 
     const getUserInfo = async () => {
       const response = await SampleApiService.get_userinfo();
-      console.log(response);
       return response.data;
     };
 
     const displayData = async () => {
-      const data = await getDay();
+      const data = await getUserInfo();
       state.userList = data;
+    };
+
+    const createDisplayExpandData = (role: any) => {
+      let result = "";
+
+      for (const key in role) {
+        result += "・" + key + ":　" + role[key] + "\n";
+      }
+      return result;
     };
 
     onMounted(async () => {
@@ -72,7 +78,14 @@ export default {
     return {
       headers,
       state,
+      createDisplayExpandData,
     };
   },
 };
 </script>
+<style scoped>
+.expand-data {
+  white-space: break-spaces;
+  font-size: 14px;
+}
+</style>
