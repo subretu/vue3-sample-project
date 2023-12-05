5fd945adc5a6fdc8ddcd3778ce43b8a9e48a4b54
miyahara <50367145+subretu@users.noreply.github.com>
2023-12-05 23:02:13 +0900

    display loading and snackbar

--- src/views/FileUpload.vue
+++ src/views/FileUpload.vue
@@ -7,20 +7,45 @@
             <v-col>
               <p>
                 <input type="file" accept="text/csv" @change="load_csv" />
-              </p>
-            </v-col>
+              </p> </v-col
+            ><v-col
+              ><v-btn variant="tonal" @click="displayLoading">
+                Upload
+              </v-btn></v-col
+            >
           </v-row>
         </v-sheet>
       </v-col>
     </v-row>
+    <v-snackbar v-model="state.snackbar" :timeout="timeout" color="primary"
+      >{{ state.snackbarMessage }}
+      <template v-slot:actions>
+        <v-btn variant="text" @click="state.snackbar = false"> Close </v-btn>
+      </template>
+    </v-snackbar>
+    <v-overlay
+      :model-value="state.isLoading"
+      :persistent="true"
+      class="align-center justify-center"
+    >
+      <v-progress-circular indeterminate color="primary" :size="64" />
+    </v-overlay>
   </v-container>
 </template>
 
 <script lang="ts">
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
   setup() {
+    const state = reactive({
+      isLoading: false,
+      snackbar: false,
+      snackbarMessage: "",
+    });
+
+    const timeout = ref(3000);
+
     const load_csv = async (event: Event) => {
       const selectedFile = event.target;
 
@@ -45,8 +70,25 @@ export default {
       return data.split("\r\n").map((row) => row.split(","));
     };
 
+    const sleep = async (ms: number) =>
+      new Promise((res) => setTimeout(res, ms));
+
+    const displayLoading = async () => {
+      state.snackbarMessage = "";
+
+      state.isLoading = true;
+      await sleep(2000);
+      state.isLoading = false;
+
+      state.snackbarMessage = "Upload Success";
+      state.snackbar = true;
+    };
+
     return {
+      state,
+      timeout,
       load_csv,
+      displayLoading,
     };
   },
 };
