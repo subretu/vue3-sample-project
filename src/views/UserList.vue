be44aa916812992e7313ab2d0c400b4c3ebc73e5
miyahara <50367145+subretu@users.noreply.github.com>
2023-12-29 14:39:35 +0900

    add csv upload button

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,7 +1,34 @@
 <template>
-  <v-container>
+  <v-container fluid>
+    <v-dialog v-model="state.dialog" width="800">
+      <v-card>
+        <v-card-title class="headline">CSV Upload </v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="10">
+              <p>
+                <v-file-input
+                  label="ファイルを選択"
+                  @change="load_csv"
+                ></v-file-input>
+              </p> </v-col
+            ><v-col cols="2"
+              ><v-btn variant="tonal" class="mt-3" @click="displayLoading">
+                Upload
+              </v-btn></v-col
+            >
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    <v-row
+      ><v-col cols="11"></v-col
+      ><v-col cols="1"
+        ><v-btn variant="tonal" @click="openDialog"> CSV Upload </v-btn></v-col
+      ></v-row
+    >
     <v-row>
-      <v-col cols="10">
+      <v-col cols="12">
         <v-sheet color="white" elevation="1">
           <v-data-table
             :headers="headers"
@@ -22,11 +49,28 @@
         </v-sheet>
       </v-col>
     </v-row>
+    <v-snackbar
+      v-model="state.snackbar"
+      :timeout="timeout"
+      :color="state.snackbarColor"
+      location="top right"
+      >{{ state.snackbarMessage }}
+      <template v-slot:actions>
+        <v-btn variant="text" @click="state.snackbar = false"> Close </v-btn>
+      </template>
+    </v-snackbar>
+    <v-overlay
+      :model-value="state.isLoading"
+      :persistent="true"
+      class="align-center justify-center"
+    >
+      <v-progress-circular indeterminate color="primary" :size="64" />
+    </v-overlay>
   </v-container>
 </template>
 
 <script lang="ts">
-import { reactive, onMounted } from "vue";
+import { reactive, ref, onMounted } from "vue";
 import SampleApiService from "../services/SampleApiService";
 
 type UserList = {
@@ -34,7 +78,34 @@ type UserList = {
   companyName: string;
 };
 
-type state = { userList: UserList[]; expanded: [] };
+type state = {
+  userList: UserList[];
+  expanded: [];
+  dialog: boolean;
+  isLoading: boolean;
+  snackbar: boolean;
+  snackbarMessage: string;
+  snackbarColor: string;
+};
+
+type fileData = {
+  fileUrl: string[];
+};
+
+const get_data_url = async (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      const result = reader.result;
+      if (typeof result === "string") {
+        resolve(result);
+      } else {
+        reject(new Error("not string"));
+      }
+    });
+    reader.readAsDataURL(file);
+  });
+};
 
 export default {
   setup() {
@@ -49,7 +120,15 @@ export default {
       },
     ];
 
-    const state = reactive<state>({ userList: [], expanded: [] });
+    const state = reactive<state>({
+      userList: [],
+      expanded: [],
+      dialog: false,
+      isLoading: false,
+      snackbar: false,
+      snackbarMessage: "",
+      snackbarColor: "green",
+    });
 
     const getUserInfo = async () => {
       const response = await SampleApiService.get_userinfo();
@@ -70,6 +149,68 @@ export default {
       return result;
     };
 
+    const openDialog = () => {
+      state.dialog = true;
+    };
+
+    const fileData = reactive<fileData>({
+      fileUrl: [],
+    });
+
+    const timeout = ref(10000);
+
+    const load_csv = async (event: Event) => {
+      fileData.fileUrl = [];
+
+      const selectedFile = event.target;
+
+      if (!(selectedFile instanceof HTMLInputElement)) return;
+      if (selectedFile.files == null || selectedFile.files.length == 0) {
+        return;
+      }
+
+      // CSVをBase64エンコード
+      for (let i = 0; i < selectedFile.files.length; i++) {
+        try {
+          const result = await get_data_url(selectedFile.files[i]);
+          fileData.fileUrl.push(result);
+        } catch (e) {
+          alert(`ERROR: ${e}`);
+        }
+      }
+
+      selectedFile.value = "";
+    };
+
+    const uploadCsv = async () => {
+      try {
+        await SampleApiService.upload_csv({
+          file: fileData.fileUrl,
+        });
+        state.isLoading = false;
+
+        state.snackbarMessage = "Upload Success";
+        state.snackbarColor = "blue";
+
+        state.snackbar = true;
+      } catch (error: any) {
+        state.isLoading = false;
+
+        state.snackbarMessage = error.response.data.message;
+        state.snackbarColor = "red";
+
+        state.snackbar = true;
+      }
+    };
+
+    const displayLoading = async () => {
+      state.snackbarMessage = "";
+
+      state.isLoading = true;
+
+      await uploadCsv();
+    };
+
     onMounted(async () => {
       await displayData();
       getUserInfo();
@@ -79,6 +220,10 @@ export default {
       headers,
       state,
       createDisplayExpandData,
+      openDialog,
+      timeout,
+      load_csv,
+      displayLoading,
     };
   },
 };
