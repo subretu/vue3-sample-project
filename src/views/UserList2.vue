21cc1931d1979e7e59fdc3e356699ae5371f03e3
miyahara <50367145+subretu@users.noreply.github.com>
2024-04-29 23:51:05 +0900

    add

--- src/views/UserList2.vue
+++ src/views/UserList2.vue
@@ -1,47 +1,105 @@
 <template>
   <v-container>
-    <v-dialog v-model="state.dialog" width="1200" scrollable>
+    <v-dialog v-model="state.dialog" width="60%" scrollable>
       <v-card title="ユーザー登録">
         <v-card-text class="pa-5">
-          <v-form v-model="valid">
+          <v-form ref="form" v-model="valid">
             <h4 class="mb-3">基本情報</h4>
-            <h5 class="mb-3">姓名</h5>
+            <h5 class="mb-3">名前</h5>
             <v-row>
-              <v-col cols="6">
+              <v-col cols="12" class="d-flex flex-row">
                 <v-text-field
                   v-model="state.userFormData.lastName"
+                  class="input-filed mr-5"
+                  density="compact"
+                  variant="outlined"
+                  color="primary"
                   label="姓"
+                  :rules="rules.lastName"
+                  type="text"
+                  required
                 ></v-text-field>
-              </v-col>
-              <v-col cols="6">
                 <v-text-field
                   v-model="state.userFormData.firstName"
+                  class="input-filed"
+                  density="compact"
+                  variant="outlined"
+                  color="primary"
                   label="名"
-                ></v-text-field
-              ></v-col>
+                  :rules="rules.firstName"
+                  type="text"
+                  required
+                ></v-text-field>
+              </v-col>
             </v-row>
-            <h5 class="mb-3">所属</h5>
+            <h5 class="mb-3 mt-3">メールアドレス</h5>
             <v-row>
-              <v-col cols="6">
+              <v-col cols="12">
+                <v-text-field
+                  v-model="state.userFormData.mailAddress"
+                  density="compact"
+                  variant="outlined"
+                  color="primary"
+                  label="メールアドレス"
+                  :rules="rules.mailAddress"
+                  type="text"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <h5 class="mb-3 mt-3">所属</h5>
+            <v-row>
+              <v-col cols="12">
                 <v-text-field
                   v-model="state.userFormData.companyName"
+                  class="input-filed2"
+                  density="compact"
+                  variant="outlined"
+                  color="primary"
                   label="会社"
+                  :rules="rules.companyName"
+                  type="text"
+                  required
                 ></v-text-field>
               </v-col> </v-row
-            ><v-divider class="mb-5"></v-divider>
+            ><v-divider class="mb-7 mt-3"></v-divider>
             <h4 class="mb-3">権限</h4>
             <v-row>
-              <v-col cols="6">
-                <v-text-field  v-model="state.userFormData.plantName" label="工場"></v-text-field
-              ></v-col>
-              <v-col cols="6">
-                <v-text-field  v-model="state.userFormData.role" label="権限"></v-text-field></v-col></v-row
+              <v-col cols="12" class="d-flex flex-row">
+                <v-text-field
+                  v-model="state.userFormData.plantName"
+                  class="input-filed mr-5"
+                  density="compact"
+                  variant="outlined"
+                  color="primary"
+                  label="工場"
+                  :rules="rules.plantName"
+                  type="text"
+                  required
+                ></v-text-field>
+                <v-select
+                  v-model="state.userFormData.role"
+                  class="input-filed"
+                  variant="outlined"
+                  color="primary"
+                  :items="items"
+                  density="compact"
+                  label="権限"
+                  required
+                ></v-select> </v-col></v-row
           ></v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn @click="insert" variant="tonal"> キャンセル </v-btn>
-          <v-btn @click="insert" variant="tonal" color="success"> 登録 </v-btn>
+          <v-btn @click="closeDialog" variant="tonal"> キャンセル </v-btn>
+          <v-btn
+            @click="registerUserFormData"
+            variant="tonal"
+            color="primary"
+            :disabled="isDisablelButton"
+          >
+            登録
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -91,8 +149,8 @@
   </v-container>
 </template>
 
-<script lang="ts">
-import { reactive, ref, onMounted, watch } from "vue";
+<script setup lang="ts">
+import { reactive, ref, onMounted, watch, computed } from "vue";
 import SampleApiService from "../services/SampleApiService";
 
 type UserList = {
@@ -104,12 +162,13 @@ type UserList = {
 type UserFormData = {
   lastName: string;
   firstName: string;
+  mailAddress: string;
   companyName: string;
   plantName: string;
   role: string;
 };
 
-type state = {
+type State = {
   userList: UserList[];
   userFormData: UserFormData;
   dialog: boolean;
@@ -117,98 +176,112 @@ type state = {
   snackbar: boolean;
   snackbarMessage: string;
   snackbarColor: string;
-  isDisablelButton: boolean;
 };
 
-export default {
-  setup() {
-    const headers = [
-      {
-        title: "ユーザー名",
-        key: "user_name",
-      },
-      {
-        title: "会社",
-        key: "company_name",
-      },
-    ];
+const form = ref(null);
+const valid = ref(false);
 
-    const state = reactive<state>({
-      userList: [],
-      userFormData: {
-        lastName: "",
-        firstName: "",
-        companyName: "",
-        plantName: "",
-        role: "",
-      },
-      dialog: false,
-      isLoading: false,
-      snackbar: false,
-      snackbarMessage: "",
-      snackbarColor: "green",
-      isDisablelButton: true,
-    });
-
-    const getUserInfo = async () => {
-      const response = await SampleApiService.get_userinfo();
-      return response.data;
-    };
-
-    const displayData = async () => {
-      const data = await getUserInfo();
-      state.userList = data;
-    };
-
-    const openDialog = () => {
-      state.dialog = true;
-    };
-
-    const timeout = ref(10000);
-
-    const displayLoading = async () => {
-      state.snackbarMessage = "";
-
-      state.isLoading = true;
-    };
-
-    const closeSnackbar = () => {
-      state.snackbar = false;
-    };
-
-    const closeDialog = () => {
-      state.dialog = false;
-    };
-
-    watch(state, () => {
-      if (!state.dialog) {
-        state.isDisablelButton = true;
-      }
-      if (state.dialog) {
-        state.isDisablelButton = true;
-      }
-    });
-
-    onMounted(async () => {
-      await displayData();
-      getUserInfo();
-    });
-
-    return {
-      headers,
-      state,
-      openDialog,
-      timeout,
-      displayLoading,
-      closeSnackbar,
-      closeDialog,
-    };
+const state = reactive<State>({
+  userList: [],
+  userFormData: {
+    lastName: "",
+    firstName: "",
+    mailAddress: "",
+    companyName: "",
+    plantName: "",
+    role: "",
   },
+  dialog: false,
+  isLoading: false,
+  snackbar: false,
+  snackbarMessage: "",
+  snackbarColor: "green",
+});
+
+const headers = [
+  {
+    title: "ユーザー名",
+    key: "user_name",
+  },
+  {
+    title: "会社",
+    key: "company_name",
+  },
+];
+
+const items = ["閲覧者", "操作員", "管理者"];
+
+const rules = {
+  lastName: [(v: string) => 1 <= v.trim().length || "姓を入力してください"],
+  firstName: [(v: string) => 1 <= v.trim().length || "名を入力してください"],
+  mailAddress: [
+    (v: string) => 1 <= v.trim().length || "メールアドレスを入力してください",
+  ],
+  companyName: [
+    (v: string) => 1 <= v.trim().length || "会社名を入力してください",
+  ],
+  plantName: [
+    (v: string) => 1 <= v.trim().length || "工場名を入力してください",
+  ],
 };
+
+const isDisablelButton = computed((): boolean => {
+  return !valid.value || state.userFormData.role.trim().length === 0;
+});
+
+const getUserInfo = async () => {
+  const response = await SampleApiService.get_userinfo();
+  return response.data;
+};
+
+const displayData = async () => {
+  const data = await getUserInfo();
+  state.userList = data;
+};
+
+const openDialog = () => {
+  state.dialog = true;
+};
+
+const timeout = ref(10000);
+
+const displayLoading = async () => {
+  state.snackbarMessage = "";
+
+  state.isLoading = true;
+};
+
+const closeSnackbar = () => {
+  state.snackbar = false;
+};
+
+const closeDialog = () => {
+  state.dialog = false;
+};
+
+const registerUserFormData = () => {
+  console.log(state.userFormData);
+  console.log(state.userFormData.role.trim().length);
+  state.dialog = false;
+};
+
+watch(state, () => {
+  if (!state.dialog) {
+    console.log("test");
+  }
+  if (state.dialog) {
+    console.log("test");
+  }
+});
+
+onMounted(async () => {
+  await displayData();
+  getUserInfo();
+});
 </script>
 <style scoped>
-.expand-data {
-  white-space: break-spaces;
-  font-size: 14px;
+.input-filed {
+  width: 100%;
 }
 </style>
+>
