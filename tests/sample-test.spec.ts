7b23f93df4f22475c7955da2f99dd40660efa7c5
miyahara <50367145+subretu@users.noreply.github.com>
2024-09-06 00:14:17 +0900

    Use getByRole

--- tests/sample-test.spec.ts
+++ tests/sample-test.spec.ts
@@ -6,12 +6,8 @@ test("open dialog", async ({ page }) => {
 
   await page.goto("http://localhost:5173/userlist");
 
-  // ダイアログを開くボタンが表示されるのを待つ
-  await page.waitForSelector('button:has-text("CSV Upload")');
-
   // ダイアログを開くボタンをクリック
-  const openDialogButton = page.locator('button:has-text("CSV Upload")');
-  await openDialogButton.click();
+  await page.getByRole("button", { name: "CSV UPLOAD" }).click();
 
   // ダイアログが表示されるのを確認
   const dialog = page.locator('div[role="dialog"] >> text="CSV Upload"');
