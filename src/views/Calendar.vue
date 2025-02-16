0a8429ad68a61b51b2c52f5fe6afcbe298326950
miyahara <50367145+subretu@users.noreply.github.com>
2025-02-17 00:34:28 +0900

    Use Biome

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -13,11 +13,11 @@
 <script setup lang="ts">
 import { ScheduleXCalendar } from "@schedule-x/vue";
 import {
-  createCalendar,
-  createViewDay,
-  createViewMonthAgenda,
-  createViewMonthGrid,
-  createViewWeek,
+	createCalendar,
+	createViewDay,
+	createViewMonthAgenda,
+	createViewMonthGrid,
+	createViewWeek,
 } from "@schedule-x/calendar";
 import "@schedule-x/theme-default/dist/index.css";
 import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
@@ -26,29 +26,29 @@ import { createResizePlugin } from "@schedule-x/resize";
 // Do not use a ref here, as the calendar instance is not reactive, and doing so might cause issues
 // For updating events, use the events service plugin
 const calendarApp = createCalendar({
-  selectedDate: "2025-02-16",
-  locale: 'ja-JP',
-  views: [
-    createViewDay(),
-    createViewWeek(),
-    createViewMonthGrid(),
-    createViewMonthAgenda(),
-  ],
-  plugins: [createDragAndDropPlugin(), createResizePlugin()],
-  events: [
-    {
-      id: 1,
-      title: "Event 1",
-      start: "2025-02-16",
-      end: "2025-02-16",
-    },
-    {
-      id: 2,
-      title: "Event 2",
-      start: "2025-02-16 13:00",
-      end: "2025-02-16 14:00",
-    },
-  ],
+	selectedDate: "2025-02-16",
+	locale: "ja-JP",
+	views: [
+		createViewDay(),
+		createViewWeek(),
+		createViewMonthGrid(),
+		createViewMonthAgenda(),
+	],
+	plugins: [createDragAndDropPlugin(), createResizePlugin()],
+	events: [
+		{
+			id: 1,
+			title: "Event 1",
+			start: "2025-02-16",
+			end: "2025-02-16",
+		},
+		{
+			id: 2,
+			title: "Event 2",
+			start: "2025-02-16 13:00",
+			end: "2025-02-16 14:00",
+		},
+	],
 });
 </script>
 <style scoped>
